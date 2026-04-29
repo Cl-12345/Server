6a19a0a3767029959694e9ab8bdfb37906742b64
Cl-12345 <Cl123456_cl@outlook.com>
2026-04-30 07:59:01 +0800

    upd26.4.30 7:58

--- chatroom/chat/views.py
+++ chatroom/chat/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Chatroom
+
+def error(req):
+    return HttpResponse('<p>你来到了未知的区域</p>')
 
 def index(req):
     return render(req, 'chat/index.html')
 
 def chat(req, roomname):
+    if not Chatroom.objects.filter(Name=roomname):
+        return error(req)
     return render(req, 'chat/chatroom.html', {'roomname': roomname})
