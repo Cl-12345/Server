b637d386c400090c8872f47f89420fbcf362df60
Cl-12345 <Cl123456_cl@outlook.com>
2026-05-07 15:21:35 +0800

    upd 26.5.7 1

--- chatroom/chatroom/asgi.py
+++ chatroom/chatroom/asgi.py
@@ -3,7 +3,7 @@ from django.core.asgi import get_asgi_application
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chatroom.routing import websocket_urlpatterns
+from chatroom.chat.routing import websocket_urlpatterns
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
