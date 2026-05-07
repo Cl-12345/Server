39b5c5ae313877d0b3feaf0bcde82c41f7998397
Cl-12345 <Cl123456_cl@outlook.com>
2026-05-07 18:18:10 +0800

    upd 26.5.7 2

--- chatroom/chat/routing.py
+++ chatroom/chat/routing.py
@@ -1,5 +1,6 @@
+# chat/routing.py
 from django.urls import re_path
-from . import consumers
+from . import consumers   # 使用相对导入
 
 websocket_urlpatterns = [
     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
--- chatroom/chatroom/asgi.py
+++ chatroom/chatroom/asgi.py
@@ -1,15 +1,18 @@
-
-from django.core.asgi import get_asgi_application
 import os
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chatroom.chat.routing import websocket_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatroom.settings')
+
+django_asgi_app = get_asgi_application()
+
+# 在这里导入 routing
+from chat.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
-        URLRouter(
-            websocket_urlpatterns
-        )
+        URLRouter(websocket_urlpatterns)
     ),
 })
